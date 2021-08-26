c381325e730f2a429405583f3c8e68c36acac655
HatDauPhong <tungtt286@gmail.com>
2021-08-26 10:25:26 +0700

    commit-update

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.js';
+import Cards from '../Cards.js';
+import HeroSection from '../HeroSection';
+
+function Home () {
+    return (
+        <div>
+            <HeroSection />
+            <Cards />
+        </div>
+    );
+}
+export default Home;
